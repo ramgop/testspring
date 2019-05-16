46a656f37f7186f72045a64668bf043c0ad80dc0
Ramesh Gopalakrishnan <ramgop@gmail.com>
2019-05-16 23:20:21 +0200

    added config message

--- src/main/java/com/ramgop/devops/basicspring/Basicspring.java
+++ src/main/java/com/ramgop/devops/basicspring/Basicspring.java
@@ -1,12 +1,15 @@
 package com.ramgop.devops.basicspring;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class Basicspring {
 
+    @Value("${test}" )
+    private String message;
     @RequestMapping("/")
     public String index() {
-        return "Greetings from DevOps Spring Boot!";}
+        return message;}
 
 }
